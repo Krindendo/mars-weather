406faf31cd8633b5c80dbaa9248b0f9082d473a0
Krindendo <lollipopfunkyx9@gmail.com>
2020-04-27 23:57:56 +0200

    update card

--- src/components/WeatherPage.js
+++ src/components/WeatherPage.js
@@ -5,25 +5,49 @@ import Card from "./Card";
 const WeatherPage = (data) => {
   return (
     <section className="weatherpage">
-      <h2>Robot: InSight</h2>
-      <h2>Service: Mars Weather</h2>
-      <h2>Location: Elysium Planitia</h2>
-      <h2>Sending data ...</h2>
-      <div className="today">
-        <div className="today__sol">
-          <h3>Sol 500</h3>
-          <h3>April 27</h3>
+      <div className="weatherpage__box">
+        <h2 className="weatherpage__box__h2">Robot: InSight</h2>
+        <h2 className="weatherpage__box__h2">
+          Mission: Daily weather measurement
+        </h2>
+        <h2 className="weatherpage__box__h2">
+          Location: Elysium Planitia, Mars
+        </h2>
+        <h2 className="weatherpage__box__h2">Sending data ...</h2>
+        <h2 className="weatherpage__box__h2 none">Data received</h2>
+        <div className="today">
+          <div className="today__sol">
+            <h3 className="today__sol__sol">Sol 500</h3>
+            <h3 className="today__sol__day">April 27</h3>
+          </div>
+          <div className="today__temperature">
+            <h3 className="today__temperature__h3">
+              High: -30{" "}
+              <span className="C active" onClick="">
+                C
+              </span>
+              <span className="slash"> | </span>
+              <span onClick="">F</span>
+            </h3>
+            <h3 className="today__temperature__h3">
+              Low: -130{" "}
+              <span className="C active" onClick="">
+                C
+              </span>
+              <span className="slash"> | </span>
+              <span onClick="">F</span>
+            </h3>
+          </div>
         </div>
-        <div className="today__temperature">
-          <h3>
-            High: <span>-30</span>
-          </h3>
-          <h3>
-            Low: <span>-130</span>
-          </h3>
+        <div className="weatherpage__box__grid">
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
         </div>
       </div>
-      <Card></Card>
     </section>
   );
 };
